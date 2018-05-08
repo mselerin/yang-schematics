466c6e36bfa2199154e135851ae40b6c53712e68
Michel Selerin <michel.selerin@outlook.com>
2018-05-08 14:10:01 +0200

    component schematic

--- src/component/index.ts
+++ src/component/index.ts
@@ -1,19 +1,22 @@
 import { Schema as ComponentOptions } from './schema';
 import {
     apply,
-    branchAndMerge,
     chain,
     FileEntry,
     forEach,
-    mergeWith, move,
+    mergeWith,
+    move, noop,
     Rule,
     SchematicContext,
+    SchematicsException,
     template,
     Tree,
     url
 } from '@angular-devkit/schematics';
 import { normalize, strings } from '@angular-devkit/core';
 import * as path from 'path';
+import { YangUtils } from '../utils/yang-utils';
+import { CodeUtils } from '../utils/code-utils';
 
 export default function (options: ComponentOptions): Rule {
     return (host: Tree, context: SchematicContext) => {
@@ -48,10 +51,87 @@ export default function (options: ComponentOptions): Rule {
 
 
         return chain([
-            mergeWith(templateSource)
-
-            // TODO Update routing
-            // TODO Update feature / shared module
+            mergeWith(templateSource),
+            addNgModule(options)
         ])(host, context);
     };
+}
+
+
+
+
+function addNgModule(options: ComponentOptions): (host: Tree) => Tree {
+    return (host: Tree) => {
+        if (options.shared) {
+            let compDir = `./components`;
+            if (!options.flat)
+                compDir += `/${strings.dasherize(options.name)}`;
+
+            const file = YangUtils.SHARED_MODULE_FILE;
+            const text = host.read(file);
+            if (text === null) {
+                throw new SchematicsException(`File ${file} does not exist.`);
+            }
+
+            const sourceText = text.toString('utf-8');
+            const sourceFile = CodeUtils.getSourceFile(file, sourceText);
+
+            CodeUtils.addImport(sourceFile,
+                `${strings.classify(options.name)}Component`, `${compDir}/${strings.dasherize(options.name)}.component`);
+
+            CodeUtils.insertInVariableArray(sourceFile, "DECLARATIONS", `   ${strings.classify(options.name)}Component`);
+            host.overwrite(file, sourceFile.getFullText());
+        }
+
+
+        else if (options.feature) {
+            let compDir = `./components`;
+            if (!options.flat)
+                compDir += `/${strings.dasherize(options.name)}`;
+
+            const file = `src/app/features/${strings.dasherize(options.feature)}/${strings.dasherize(options.feature)}.module.ts`;
+            const text = host.read(file);
+            if (text === null) {
+                throw new SchematicsException(`File ${file} does not exist.`);
+            }
+
+            const sourceText = text.toString('utf-8');
+            const sourceFile = CodeUtils.getSourceFile(file, sourceText);
+
+            CodeUtils.addImport(sourceFile,
+                `${strings.classify(options.name)}Component`, `${compDir}/${strings.dasherize(options.name)}.component`);
+
+            CodeUtils.insertInVariableArray(sourceFile, "DECLARATIONS", `   ${strings.classify(options.name)}Component`);
+            host.overwrite(file, sourceFile.getFullText());
+
+            updateFeatureRouting(options, host);
+        }
+
+        return host;
+    };
+}
+
+function updateFeatureRouting(options: ComponentOptions, host: Tree): void {
+    // Ajouter la route
+    const file = `src/app/features/${strings.dasherize(options.feature)}/${strings.dasherize(options.feature)}-routing.module.ts`;
+    const text = host.read(file);
+    if (text === null) {
+        throw new SchematicsException(`File ${file} does not exist.`);
+    }
+
+    const sourceText = text.toString('utf-8');
+    const sourceFile = CodeUtils.getSourceFile(file, sourceText);
+
+    let compDir = `.`;
+    if (!options.flat)
+        compDir += `/${strings.dasherize(options.name)}`;
+
+    CodeUtils.addImport(sourceFile,
+        `${strings.classify(options.name)}Component`, `${compDir}/${strings.dasherize(options.name)}.component`);
+
+    CodeUtils.insertInVariableArray(sourceFile, `${strings.classify(options.feature)}Routes`,
+        `    { path: '${strings.dasherize(options.name)}', component: '${strings.classify(options.name)}Component' }`
+    );
+
+    host.overwrite(file, sourceFile.getFullText());
 }
