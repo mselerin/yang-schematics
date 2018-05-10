760b748afa7f2dda7ab3da77f77fc5065179b369
Michel Selerin <michel.selerin@outlook.com>
2018-05-10 23:59:25 +0200

    feat: merging header and footer inside layout

--- src/init/files/root/src/app/features/layout/layout.component.ts
+++ src/init/files/root/src/app/features/layout/layout.component.ts
@@ -1,15 +1,38 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
     selector: 'app-layout',
     template: `
-        <app-header></app-header>
+        <header id="main-header">
+            <p>
+                <select #langSelect (change)="translate.use(langSelect.value)">
+                    <option *ngFor="let lang of translate.getLangs()" [value]="lang" [selected]="lang === translate.currentLang">{{ lang }}</option>
+                </select>
+            </p>
+        </header>
 
         <div id="main-container">
             <router-outlet></router-outlet>
         </div>
 
-        <app-footer></app-footer>
+        <footer id="main-footer">
+            Version : {{ manifest.version }}
+            - Build date : {{ manifest.buildDate | date:'dd-MM-y' }}
+        </footer>
     `
 })
-export class LayoutComponent { }
+export class LayoutComponent implements OnInit
+{
+    public manifest: any = {};
+
+    constructor(
+        public translate: TranslateService,
+        private http: HttpClient
+    ) {}
+
+    async ngOnInit() {
+        this.manifest = await this.http.get('assets/app-manifest.json').toPromise();
+    }
+}
