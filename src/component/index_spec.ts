aa2fcfead9fe8f89ddb7b035a782008f682be5c0
Michel Selerin <michel.selerin@outlook.com>
2018-05-17 23:57:26 +0200

    test: unit-test for component and directive

---
+++ src/component/index_spec.ts
@@ -0,0 +1,14 @@
+import { expect } from 'chai';
+import { Tree } from '@angular-devkit/schematics';
+import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import * as path from 'path';
+
+const collectionPath = path.join(__dirname, '../collection.json');
+
+
+describe('yang-schematics-component', () => {
+    it('should throw error on empty tree', () => {
+        const runner = new SchematicTestRunner('schematics', collectionPath);
+        expect(() => runner.runSchematic('component', {}, Tree.empty())).to.throw();
+    });
+});
---
+++ src/directive/index_spec.ts
@@ -0,0 +1,14 @@
+import { expect } from 'chai';
+import { Tree } from '@angular-devkit/schematics';
+import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import * as path from 'path';
+
+const collectionPath = path.join(__dirname, '../collection.json');
+
+
+describe('yang-schematics-directive', () => {
+    it('should throw error on empty tree', () => {
+        const runner = new SchematicTestRunner('schematics', collectionPath);
+        expect(() => runner.runSchematic('directive', {}, Tree.empty())).to.throw();
+    });
+});
