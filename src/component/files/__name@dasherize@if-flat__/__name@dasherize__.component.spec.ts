7722c270946c8fa50b5c1567d0fc6d2cf4d443fb
Michel Selerin <michel.selerin@outlook.com>
2018-06-14 17:40:05 +0200

    fix: unit test for component should use NO_ERROR_SCHEMA and not import modules

--- src/component/files/__name@dasherize@if-flat__/__name@dasherize__.component.spec.ts
+++ src/component/files/__name@dasherize@if-flat__/__name@dasherize__.component.spec.ts
@@ -1,28 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { <%=classify(name)%>Component } from './<%=dasherize(name)%>.component';
-import { CoreModule } from '@app/core/core.module';
-import { SharedModule } from '@app/shared/shared.module';
 
 describe('<%=classify(name)%>Component', () => {
-    let component: <%=classify(name)%>Component;
-    let fixture: ComponentFixture<<%=classify(name)%>Component>;
+  let component: <%=classify(name)%>Component;
+  let fixture: ComponentFixture<<%=classify(name)%>Component>;
 
-    beforeEach(async(() => {
-        TestBed.configureTestingModule({
-            imports: [ CoreModule, SharedModule ],
-            declarations: [ <%=classify(name)%>Component ]
-        })
-        .compileComponents();
-    }));
-
-    beforeEach(() => {
-        fixture = TestBed.createComponent(<%=classify(name)%>Component);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ <%=classify(name)%>Component ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
-    });
+    TestBed.compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(<%=classify(name)%>Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
