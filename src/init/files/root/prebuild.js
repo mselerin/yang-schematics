e275853dca84878ffb292e2913575b14ff704b48
Michel Selerin <michel.selerin@outlook.com>
2018-10-15 20:33:08 +0200

    fix: prebuild.js not included

---
+++ src/init/files/root/prebuild.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const path = require('path');
+
+const PKG = require("./package.json");
+const BUILD_DATE = new Date();
+
+const MANIFEST = {
+  name: `${PKG.name}`,
+  version: `${PKG.version}`,
+  buildDate: `${BUILD_DATE.toISOString()}`
+};
+
+const manifestFile = path.join('src', 'assets', 'app-manifest.json');
+
+// Write environment file
+fs.writeFileSync(manifestFile, JSON.stringify(MANIFEST, null, 2));
+console.log('\x1b[32m%s\x1b[0m', `Manifest file written in ${manifestFile}`);
