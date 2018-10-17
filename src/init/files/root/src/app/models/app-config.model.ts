5544225634c3c4c6bc6e5ccc6f0aa65cf65078f4
Michel Selerin <michel.selerin@outlook.com>
2018-10-17 12:54:22 +0200

    feat: enhanced config service

--- src/init/files/root/src/app/models/app-config.model.ts
+++ src/init/files/root/src/app/models/app-config.model.ts
@@ -1,6 +1,6 @@
-export class AppConfig
+export interface AppConfig
 {
-  lang: string = 'fr';
-  languages: string[] = ['fr', 'en'];
-  multiLang: boolean = true;
+  lang?: string;
+  languages?: string[];
+  multiLang?: boolean;
 }
--- src/init/files/root/src/app/services/config.service.ts
+++ src/init/files/root/src/app/services/config.service.ts
@@ -1,22 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { AppConfig } from '@app/models/app-config.model';
 
 @Injectable({ providedIn: 'root' })
 export class ConfigService
 {
-  public app: AppConfig;
+  [key: string]: any;
 
-  constructor(
+  constructor (
     private http: HttpClient
   ) {
-    this.app = new AppConfig();
+    this.app = {};
   }
 
   async loadAppConfig(): Promise<void> {
-    let url = 'assets/config/app-config.json';
-    let data = await this.http.get(url).toPromise();
+    await this.loadConfig('app', 'assets/config/app-config.json');
+  }
 
-    Object.assign(this.app, new AppConfig(), data);
+  async loadConfig(prefix: string, url: string) {
+    this[prefix] = await this.http.get(url).toPromise();
+  }
+
+  get(key: string, def: any = null): any {
+    const value = key.split('.').reduce((o, i) => (o != undefined && o != null) ? o[i] : null, this);
+    return (value !== null && value !== undefined ? value : def);
   }
 }
