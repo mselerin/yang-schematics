4ad70856d90fe2b8d13851ae704d9e89f8270556
Michel Selerin <michel.selerin@outlook.com>
2018-10-17 15:18:41 +0200

    feat: better default application styles & layout

--- src/init/files/root/src/app/layouts/main/layout.component.ts
+++ src/init/files/root/src/app/layouts/main/layout.component.ts
@@ -4,31 +4,15 @@ import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-layout',
-  template: `
-    <header id="main-header">
-      <p>
-        <select #langSelect (change)="translate.use(langSelect.value)">
-          <option *ngFor="let lang of translate.getLangs()" [value]="lang" [selected]="lang === translate.currentLang">{{ lang }}</option>
-        </select>
-      </p>
-    </header>
-
-    <div id="main-container">
-      <router-outlet></router-outlet>
-    </div>
-
-    <footer id="main-footer">
-      Version : {{ manifest.version }}
-      - Build date : {{ manifest.buildDate | date:'dd-MM-y' }}
-    </footer>
-    `
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent implements OnInit
 {
   public manifest: any = {};
 
   constructor(
-    public translate: TranslateService,
+    public i18n: TranslateService,
     private http: HttpClient
   ) {}
 
