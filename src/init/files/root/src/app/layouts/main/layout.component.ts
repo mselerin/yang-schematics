3f49e6764a6e5f9c48e57e1db0454f494bb92cc6
Michel Selerin <michel.selerin@outlook.com>
2018-11-28 15:12:59 +0100

    chore: remove HttpClient import inside layout

--- src/init/files/root/src/app/layouts/main/layout.component.ts
+++ src/init/files/root/src/app/layouts/main/layout.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { TranslateService } from '@ngx-translate/core';
 import { LOGGER } from '@app/services/logger.service';
 
