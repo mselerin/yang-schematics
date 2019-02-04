19b8b46798e37857b03111a77f9d3a2e775dff12
Michel Selerin <Michel.Selerin@nsi-sa.be>
2019-02-04 16:19:32 +0100

    chore: update to angular-cli 7.3

--- src/utils/test-utils.ts
+++ src/utils/test-utils.ts
@@ -1,6 +1,7 @@
 import * as path from "path";
-import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
-import { Schema as NgNewOptions } from '../ng-new/schema';
+import {SchematicTestRunner, UnitTestTree} from '@angular-devkit/schematics/testing';
+import {Schema as NgNewOptions} from '../ng-new/schema';
+import {Style} from "@schematics/angular/ng-new/schema";
 
 export const yangSchematicRunner = new SchematicTestRunner(
   'yang-schematics', path.join(__dirname, '../collection.json')
@@ -10,7 +11,7 @@ export const ngNewOptions: NgNewOptions = {
   name: 'foo',
   directory: '.',
   version: '7.0.0',
-  style: 'scss'
+  style: Style.Scss
 };
 
 export function runYangNew(): UnitTestTree {
