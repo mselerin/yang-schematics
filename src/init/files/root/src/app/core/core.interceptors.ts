c38c10733422c0fcf9f9f676298c8a26bc7fb157
Michel Selerin <Michel.Selerin@nsi-sa.be>
2019-05-10 10:54:41 +0200

    chore: code style

--- src/init/files/root/src/app/core/core.interceptors.ts
+++ src/init/files/root/src/app/core/core.interceptors.ts
@@ -14,14 +14,16 @@ import {Observable} from 'rxjs';
 const API_URL_TOKEN = "/api/";
 
 @Injectable()
-export class APIInterceptor implements HttpInterceptor
-{
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>
-  {
-    if (environment.apiUrl && environment.apiUrl !== API_URL_TOKEN && req.url.startsWith(API_URL_TOKEN)) {
-      let url = environment.apiUrl + req.url.substr(API_URL_TOKEN.length);
+export class APIInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (req.url.startsWith(API_URL_TOKEN)) {
+      const url = (environment.apiUrl && environment.apiUrl !== API_URL_TOKEN) ?
+        environment.apiUrl + req.url.substr(API_URL_TOKEN.length) :
+        req.url;
+
       req = req.clone({
-        url: url
+        url: url,
+        withCredentials: true
       });
     }
 
