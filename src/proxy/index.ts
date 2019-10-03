aefa41cf9cb2f9899d25441bd0dc0d1f1031dd06
Selerin Michel <Michel.Selerin@nsi-sa.be>
2019-10-03 16:10:03 +0200

    feat: add proxy schematics

---
+++ src/proxy/index.ts
@@ -0,0 +1,49 @@
+import { Schema as ProxyOptions } from './schema';
+import {
+  apply,
+  chain,
+  MergeStrategy,
+  mergeWith,
+  move,
+  Rule,
+  schematic,
+  SchematicContext,
+  SchematicsException,
+  template,
+  Tree,
+  url
+} from '@angular-devkit/schematics';
+import { getWorkspace, updateWorkspace } from '@schematics/angular/utility/config';
+
+export default function (options: ProxyOptions): Rule {
+  return (host: Tree, context: SchematicContext) => {
+    return chain([
+      mergeWith(apply(url('./files'), [
+        template({
+          ...options
+        }),
+        move(''),
+      ]), MergeStrategy.Overwrite),
+      updateProjectWorkspace()
+    ])(host, context);
+  };
+}
+
+
+function updateProjectWorkspace(): (host: Tree) => Rule {
+  return (host: Tree) => {
+    const workspace = getWorkspace(host);
+    const project = workspace.defaultProject as string;
+
+    const architect = workspace.projects[project].architect;
+    if (!architect) throw new Error(`expected node projects/${project}/architect in angular.json`);
+
+    // Add proxy config
+    const serve = architect.serve;
+    if (!serve) throw new Error(`expected node projects/${project}/architect/serve in angular.json`);
+
+    (<any>serve.options)['proxyConfig'] = 'proxy.conf.json';
+
+    return updateWorkspace(workspace);
+  }
+}
---
+++ src/proxy/index_spec.ts
@@ -0,0 +1,50 @@
+import { Tree } from '@angular-devkit/schematics';
+import { runYangNew, yangSchematicRunner } from '../utils/test-utils';
+import { UnitTestTree } from '@angular-devkit/schematics/testing';
+import { Schema as ProxyOptions } from '../proxy/schema';
+import { strings } from '@angular-devkit/core';
+import { YangUtils } from '../utils/yang-utils';
+import { getFileContent } from '@schematics/angular/utility/test';
+
+
+const elementName = 'superDummy';
+const defaultOptions: ProxyOptions = {};
+
+describe('Proxy Schematic', () => {
+  describe('With empty project', () => {
+    it('should throw error on empty tree', () => {
+      return expect(yangSchematicRunner.runSchematicAsync('proxy', {}, Tree.empty()).toPromise()).rejects.toThrow();
+    });
+  });
+
+  describe('With fresh project', () => {
+    let appTree: UnitTestTree;
+    beforeEach(async () => {
+      appTree = await runYangNew().toPromise();
+    });
+
+    describe('With default options', () => {
+      beforeEach(async () => {
+        appTree = await yangSchematicRunner.runSchematicAsync('proxy', defaultOptions, appTree).toPromise();
+      });
+
+      it('should create files inside app', () => {
+        const files = appTree.files;
+        expect(files).toContain(`/proxy.conf.json`);
+      });
+
+
+      it('should add proxy config inside angular.json', () => {
+        const fileContent = getFileContent(appTree, '/angular.json');
+        const json = JSON.parse(fileContent);
+        const defaultProject = json.defaultProject;
+        let architect = json.projects[defaultProject].architect;
+        if (!architect)
+          architect = json.projects[defaultProject].targets;
+
+        expect(architect.serve.options.proxyConfig).toEqual('proxy.conf.json');
+      });
+    });
+
+  });
+});
