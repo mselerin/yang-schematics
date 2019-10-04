5672c0af9b36ce2160fcc75a89525577c9201e93
Selerin Michel <Michel.Selerin@nsi-sa.be>
2019-10-04 12:29:47 +0200

    fix: e2e generation

--- src/init/files/root/e2e/src/app.e2e-spec.ts
+++ src/init/files/root/e2e/src/app.e2e-spec.ts
@@ -1,6 +1,7 @@
 import { AppPage } from './app.po';
+import { browser, logging } from 'protractor';
 
-describe('<%=name%> App', () => {
+describe('workspace-project App', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -9,6 +10,14 @@ describe('<%=name%> App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).not.toBe('');
+    expect(page.getTitleText()).not.toBe('');
+  });
+
+  afterEach(async () => {
+    // Assert that there are no errors emitted from the browser
+    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+    expect(logs).not.toContain(jasmine.objectContaining({
+      level: logging.Level.SEVERE,
+    } as logging.Entry));
   });
 });
