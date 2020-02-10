7999f9e8eaae224830d7f47e83463ef2850180b7
Michel Selerin <michel.selerin@outlook.com>
2020-02-10 12:11:24 +0100

    chore: update deps

--- src/jest/dependencies.ts
+++ src/jest/dependencies.ts
@@ -1,6 +1,6 @@
 export const extraDevDependencies = {
   '@angular-builders/jest': '~8.3.0',
-  '@testing-library/jest-dom': '~4.2.0',
+  '@testing-library/jest-dom': '~5.1.0',
   '@types/jest': '~25.1.0',
   'jest': '~25.1.0',
   'jest-preset-angular': '~8.0.0',
