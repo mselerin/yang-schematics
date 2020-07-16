e7f50b7a4ae3c4a7dddbab35c342f7f0a151c587
Michel Selerin <Michel.Selerin@nsi-sa.be>
2020-07-16 17:21:20 +0200

    feat: adding a BaseModule for the AppComponent & Layouts

--- src/init/files/root/src/app/shared/shared.module.ts
+++ src/init/files/root/src/app/shared/shared.module.ts
@@ -1,21 +1,16 @@
 /**
  * The SharedModule should only contains modules, components, directives and pipes
- * that are shared across every modules & features.
+ * that are shared across every features.
  *
- * Do NOT import too much modules here to prevent big vendor.js files.
+ * Do NOT import too much modules here to prevent budgets errors.
  * Prefer importing external modules inside the features directly.
  */
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {RouterModule} from '@angular/router';
+import {BaseModule} from './base.module';
 
 
 const MODULES: any[] = [
-  CommonModule,
-  FormsModule,
-  ReactiveFormsModule,
-  RouterModule
+  BaseModule
 ];
 
 const DECLARATIONS: any[] = [
