c87ac4fb57e02bc4b7c603711d6eb3f9aa6eec5f
Michel Selerin <Michel.Selerin@nsi-sa.be>
2020-07-16 13:06:36 +0200

    chore: deps update

--- src/init/dependencies.ts
+++ src/init/dependencies.ts
@@ -1,8 +1,7 @@
 export const extraDependencies = {
-  'whatwg-fetch': '~3.1.0',
-  '@ngx-translate/core': '~13.0.0'
+  'whatwg-fetch': '~3.2.0'
 };
 
 export const extraDevDependencies = {
-  'ngx-build-plus': '~9.0.6'
+  'ngx-build-plus': '~10.1.1'
 };
