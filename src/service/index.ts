271bc4ed59c6579eb27fb48b2149b4f2ddcb9ff9
Michel Selerin <Michel.Selerin@nsi-sa.be>
2020-11-15 23:19:20 +0100

    feat(service): allow global configuration for flat and skipImport parameters

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,10 +1,10 @@
 import {Schema as ServiceOptions} from './schema';
 import {chain, externalSchematic, Rule, Tree} from '@angular-devkit/schematics';
 import {parseName} from '@schematics/angular/utility/parse-name';
-import {getSourceRoot} from '../utils/yang-utils';
+import {getProjectSchematic, getSourceRoot} from '../utils/yang-utils';
 
 export default function (options: ServiceOptions): Rule {
-  return (host: Tree) => {
+  return async (host: Tree) => {
     const rootPath = getSourceRoot(host, options);
 
     if (!options.path) {
@@ -14,6 +14,10 @@ export default function (options: ServiceOptions): Rule {
     const parsedPath = parseName(options.path, options.name);
     options.name = parsedPath.name;
     options.path = parsedPath.path;
+
+    const schematic = await getProjectSchematic(host, options, '@schematics/angular:service');
+    options.flat = options.flat ?? schematic.flat ?? true;
+    options.skipTests = options.skipTests ?? schematic.skipTests ?? false;
 
     return chain([
       externalSchematic('@schematics/angular', 'service', options)
